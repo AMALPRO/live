9ace38657b0337041d16abbac2d8295339f98956
AMALPRO <87407213+AMALPRO@users.noreply.github.com>
2024-06-17 17:39:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,11 @@
 
 ffmpeg -re -fflags +genpts \
        -i "https://segment.yuppcdn.net/110322/channel24/playlist.m3u8" \
-       -vf "scale=-2:1080" \
-       -c:v libx264 -b:v 4500K \
+       -vf "scale=-2:720" \
+       -c:v libx264 -preset veryfast -tune zerolatency -crf 23 \
        -c:a aac -b:a 128k \
        -buffer_size 8192k \
+       -max_muxing_queue_size 1024 \
        -rw_timeout 10000000 \
        -strict -2 \
        -f flv "rtmp://a.rtmp.youtube.com/live2/8vt3-84u0-kpcy-fk7r-cr49"
