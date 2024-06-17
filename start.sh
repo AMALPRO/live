f646c67a9be169f980d2ec3231baa88cec71541f
AMALPRO <87407213+AMALPRO@users.noreply.github.com>
2024-06-17 17:24:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-ffmpeg -i "https://segment.yuppcdn.net/110322/channel24/playlist.m3u8" \
-       -i watermark.png \   # Assuming watermark.png is in the same directory
-       -filter_complex "[0:v][1:v] overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/1.1" \
+ffmpeg -re -fflags +genpts \
+       -i "https://segment.yuppcdn.net/110322/channel24/playlist.m3u8" \
        -vf "scale=-2:1080" \
        -c:v libx264 -b:v 4500K \
        -c:a aac -b:a 128k \
-       -buffer_size 2048k \
-       -rw_timeout 5000000 \
+       -buffer_size 8192k \
+       -rw_timeout 10000000 \
+       -strict -2 \
        -f flv "rtmp://a.rtmp.youtube.com/live2/8vt3-84u0-kpcy-fk7r-cr49"
