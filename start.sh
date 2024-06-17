d3cf9c50514e3c58f41921ac2341050d76e49145
AMALPRO <87407213+AMALPRO@users.noreply.github.com>
2024-06-17 18:15:46 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 ffmpeg -re -fflags +genpts \
        -i "https://segment.yuppcdn.net/110322/channel24/playlist.m3u8" \
-       -vf "scale=-2:720" \
+       -vf "scale=-2:720, drawtext=text='watchasports.in':fontsize=36:fontcolor=white:x=(w-text_w)/2:y=(h-text_h-50)" \
        -c:v libx264 -preset veryfast -tune zerolatency -crf 23 \
        -c:a aac -b:a 128k \
        -buffer_size 8192k \
