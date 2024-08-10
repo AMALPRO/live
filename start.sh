ac0ed2613f7e8a9c1696d4e2d18a7030f4e8f0d2
AMALPRO <87407213+AMALPRO@users.noreply.github.com>
2024-08-10 12:06:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
-# Download the logo from the URL
-curl -o /tmp/logo.png "https://i.ibb.co/5GY0CVT/logo.png"
-
 while true
 do
     ffmpeg -re -fflags +genpts \
-           -i "https://allinonereborn.in/sliv.m3u8?ch=TEN2HD&id=2020434" \
-           -i "/tmp/logo.png" -filter_complex \
-           "[0:v][1:v] overlay=W-w-10:H-h-10, \
+           -i "http://thunderbolt-40gbps.com/play/VSZx3MGCJpLbqnmI5wjgPBzuaa0kdmjV0U027_Fvcam-nHLL8BKS5m2JBotp0mi5" \
+           -vf "scale=-2:720, \
     drawtext=text='watchasports.in':fontsize=38:fontcolor=white:x=(w-text_w)/2:y=(h-text_h-90), \
     drawtext=text='watchasports':fontsize=25:fontcolor=white:x=(w-text_w-30):y=30" \
            -c:v libx264 -preset veryfast -tune zerolatency -crf 23 \
