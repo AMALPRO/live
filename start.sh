8d3b6c3516a1890f331c06e2e45a666a12571724
AMALPRO <87407213+AMALPRO@users.noreply.github.com>
2024-08-10 12:42:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,10 +3,13 @@
 while true
 do
     ffmpeg -re -fflags +genpts \
-           -i "http://thunderbolt-40gbps.com/play/VSZx3MGCJpLbqnmI5wjgPBzuaa0kdmjV0U027_Fvcam-nHLL8BKS5m2JBotp0mi5" \
-           -vf "scale=-2:720, \
-    drawtext=text='watchasports.in':fontsize=38:fontcolor=white:x=(w-text_w)/2:y=(h-text_h-90), \
-    drawtext=text='watchasports':fontsize=25:fontcolor=white:x=(w-text_w-30):y=30" \
+           -i "http://50.7.218.122:8080/gemjunior/index.m3u8" \
+           -i "https://i.ibb.co/5GY0CVT/logo.png" \
+           -filter_complex " \
+           [1:v]scale=100:-1[logo]; \
+           [0:v]scale=-2:720[v0]; \
+           [v0][logo]overlay=x=W-w-30:y=30, \
+           drawtext=text='watchasports.in':fontsize=38:fontcolor=white:x=(w-text_w)/2:y=(h-text_h-90)" \
            -c:v libx264 -preset veryfast -tune zerolatency -crf 23 \
            -maxrate 2000k -bufsize 4000k \
            -c:a aac -b:a 128k \
