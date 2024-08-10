6f80c12c816aacb9ef5a7f297a8cae3875f4c57c
AMALPRO <87407213+AMALPRO@users.noreply.github.com>
2024-08-10 11:21:02 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
+# Download the logo from the URL
+curl -o /tmp/logo.png "https://i.ibb.co/5GY0CVT/logo.png"
+
 while true
 do
     ffmpeg -re -fflags +genpts \
-           -i "http://thunderbolt-40gbps.com/play/VSZx3MGCJpLbqnmI5wjgPBzuaa0kdmjV0U027_Fvcam-nHLL8BKS5m2JBotp0mi5" \
-           -vf "scale=-2:720, \
+           -i "https://allinonereborn.in/sliv.m3u8?ch=TEN2HD&id=2020434" \
+           -i "/tmp/logo.png" -filter_complex \
+           "[0:v][1:v] overlay=W-w-10:H-h-10, \
     drawtext=text='watchasports.in':fontsize=38:fontcolor=white:x=(w-text_w)/2:y=(h-text_h-90), \
     drawtext=text='watchasports':fontsize=25:fontcolor=white:x=(w-text_w-30):y=30" \
            -c:v libx264 -preset veryfast -tune zerolatency -crf 23 \
            -maxrate 2000k -bufsize 4000k \
            -c:a aac -b:a 128k \
-           -f flv "rtmp://live.restream.io/live/re_8087709_93a2c94f3e471c11a179"
+           -f flv "rtmp://live.restream.io/live/re_8087088_bad11148233976dd9d57"
 
     # Wait for 5 seconds before restarting if the command exits
     sleep 5
